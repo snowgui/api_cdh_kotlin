97859876b2e14a20b87731460ae34619a44c4901
Guilherme <guilherme42correa@gmail.com>
2023-10-17 23:02:45 -0300

    ADD: completeWithVale string methods

--- src/main/kotlin/com/guicgon/api_cdh_kotlin/api_cdh_kotlin/adapter/in/controller/StringController.kt
+++ src/main/kotlin/com/guicgon/api_cdh_kotlin/api_cdh_kotlin/adapter/in/controller/StringController.kt
@@ -8,8 +8,32 @@ import org.springframework.web.bind.annotation.*
 class StringController {
 
     @GetMapping("")
-    fun get() : ResponseEntity<String>{
+    fun get(): ResponseEntity<String> {
         return ResponseEntity.ok().body("Hello World!")
     }
+    @GetMapping("completeWithValue/{len}/{value}", "completeWithValue/{len}")
+    fun completeWithValue(
+        @PathVariable len: Int,
+        @PathVariable value: String? = "",
+    ): ResponseEntity<String> {
 
+        var strLen = ""
+        val str = value ?: ""
+        for (x in 1..len) strLen += "0"
+
+        val response = ("$strLen$str").substring(str.length)
+        // val response ("0000$str").substring(str.length)
+
+        return ResponseEntity.ok().body(response)
+
+    }
+    @GetMapping("completeWithFourZeroValue/{value}")
+    fun completeWithFourZeroValue(
+        @PathVariable value: String,
+    ): ResponseEntity<String> = ResponseEntity.ok().body("0000$value".substring(value.length))
+    @GetMapping("completeWithFourZeroValueInt/{value}")
+    fun completeWithFourZeroValueInt(
+        @PathVariable value: Int,
+    ): ResponseEntity<String> = ResponseEntity.ok().body(String.format("%04d", value))
+    
 }
