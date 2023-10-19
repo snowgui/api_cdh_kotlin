da83c812bbab51d9f2d4c6632ba609ee284ae8dc
Guilherme Correa <guilherme@smit.net.br>
2023-10-18 23:13:41 -0300

    ADD: field in Carro obj

--- src/main/kotlin/com/guicgon/api_cdh_kotlin/api_cdh_kotlin/adapter/in/model/obj/Carro.kt
+++ src/main/kotlin/com/guicgon/api_cdh_kotlin/api_cdh_kotlin/adapter/in/model/obj/Carro.kt
@@ -2,6 +2,7 @@ package com.guicgon.api_cdh_kotlin.api_cdh_kotlin.adapter.`in`.model.obj
 
 data class Carro(
     val marca: String,
-    val modelo: String
+    val modelo: String,
+    val Ano: Int
 
 )
